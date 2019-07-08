152d3a842911b7105916c441a818b5ad31bcb1e5
陈耀龙 <15202156764@163.com>
2019-07-08 12:42:17 +0800

    删除不需要的配置

--- app/models/classic.js
+++ app/models/classic.js
@@ -4,24 +4,6 @@ const {
     Model
 } = require('sequelize')
 
-
-// 本地 跑起来
-// 另外一台电脑 跑起来
-// 本地电脑外网ip
-
-// 云服务器 Linux 电脑
-// localhost , ip
-// 域名，注册域名
-// 备案
-// 解析 7yue.pro ip
-
-// mysql node xampp linux
-// localhost:3000
-
-// ip:3000
-// http://7yue.pro:3000/v1/classic/...
-
-
 const classicFields = {
     image: {
         type:Sequelize.STRING,
